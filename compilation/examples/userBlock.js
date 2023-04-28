3116611e7ffd1f1727efa6b47dbd6aada0f37ebb
ByLife <bylife@bylife.fr>
2023-04-28 22:11:45 +0200

    Update import

--- compilation/examples/userBlock.js
+++ compilation/examples/userBlock.js
@@ -3,8 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const src_1 = __importDefault(require("../src"));
-const client = new src_1.default.Client(); // Create a new client
+const __1 = __importDefault(require("../"));
+const client = new __1.default.Client(); // Create a new client
 (async () => {
     await client.login("441A9FEFE7AA543C8B99F44A7A05819E1676727077194"); // luxlux2 account token
     const userToBlockID = 1678272658965; // ThomasJan account ID
